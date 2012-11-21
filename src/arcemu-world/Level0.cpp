3e848a5d076260c912326caaea0a3a92622bcc00
ZeroocoolDev <hacknets@hotmail.com>
2012-11-21 18:15:56 +0100

    Added some info in server info command.

--- src/arcemu-world/Level0.cpp
+++ src/arcemu-world/Level0.cpp
@@ -25,7 +25,7 @@
 #include "StdAfx.h"
 #include <git_version.h>
 
-bool ChatHandler::ShowHelpForCommand(WorldSession* m_session, ChatCommand* table, const char* cmd)
+bool ChatHandler::ShowHelpForCommand(WorldSession *m_session, ChatCommand *table, const char* cmd)
 {
 	for(uint32 i = 0; table[i].Name != NULL; i++)
 	{
@@ -56,7 +56,7 @@ bool ChatHandler::ShowHelpForCommand(WorldSession* m_session, ChatCommand* table
 	return false;
 }
 
-bool ChatHandler::HandleHelpCommand(const char* args, WorldSession* m_session)
+bool ChatHandler::HandleHelpCommand(const char* args, WorldSession *m_session)
 {
 //	ChatCommand *table = getCommandTable();
 	WorldPacket data;
@@ -70,22 +70,22 @@ bool ChatHandler::HandleHelpCommand(const char* args, WorldSession* m_session)
 
 	if(!ShowHelpForCommand(m_session, CommandTableStorage::getSingleton().Get(), cmd))
 	{
-		RedSystemMessage(m_session, "Sorry, no help was found for this command, or that command does not exist.");
+		SystemMessage(m_session, "Sorry, no help was found for this command, or that command does not exist.");
 	}
 
 	return true;
 }
 
 
-bool ChatHandler::HandleCommandsCommand(const char* args, WorldSession* m_session)
+bool ChatHandler::HandleCommandsCommand(const char* args, WorldSession *m_session)
 {
-	ChatCommand* table = CommandTableStorage::getSingleton().Get();
+	ChatCommand *table = CommandTableStorage::getSingleton().Get();
 	WorldPacket data;
 
 	std::string output;
 	uint32 count = 0;
 
-	output = "Available commands: \n\n";
+	output = "Available GM commands \n\n";
 
 	for(uint32 i = 0; table[i].Name != NULL; i++)
 	{
@@ -97,34 +97,33 @@ bool ChatHandler::HandleCommandsCommand(const char* args, WorldSession* m_sessio
 
 		switch(table[i].CommandGroup)
 		{
-			case 'z':
-				{
-					output += "|cffff6060";
-					output += table[i].Name;
-					output += "|r, ";
-				}
-				break;
-			case 'm':
-				{
-					output += "|cff00ffff";
-					output += table[i].Name;
-					output += ", ";
-				}
-				break;
-			case 'c':
-				{
-					output += "|cff00ff00";
-					output += table[i].Name;
-					output += "|r, ";
-				}
-				break;
-			default:
-				{
-					output += "|cff00ccff";
-					output += table[i].Name;
-					output += "|r, ";
-				}
-				break;
+		case 'z':
+			{
+				output+="|cffff6060";
+				output+=table[i].Name;
+				output+="|r, ";
+			}
+			break;
+		case 'm':
+			{
+				output+="|cff00ffff";
+				output+=table[i].Name;
+				output+=", ";
+			}
+			break;
+		case 'c':
+			{
+				output += "|cff00ff00";
+				output += table[i].Name;
+				output += "|r, ";
+			}break;
+		default:
+			{
+				output+="|cff00ccff";
+				output+=table[i].Name;
+				output+="|r, ";
+			}
+			break;
 		}
 
 		count++;
@@ -138,8 +137,8 @@ bool ChatHandler::HandleCommandsCommand(const char* args, WorldSession* m_sessio
 		output += "\n";
 
 
-	//FillSystemMessageData(&data, table[i].Name);
-	//m_session->SendPacket(&data);
+		//FillSystemMessageData(&data, table[i].Name);
+		//m_session->SendPacket(&data);
 	//}
 
 	SendMultilineMessage(m_session, output.c_str());
@@ -147,26 +146,70 @@ bool ChatHandler::HandleCommandsCommand(const char* args, WorldSession* m_sessio
 	return true;
 }
 
-bool ChatHandler::HandleStartCommand(const char* args, WorldSession* m_session)
+bool ChatHandler::HandleStartCommand(const char* args, WorldSession *m_session)
 {
 	std::string race;
-	Player* m_plyr = getSelectedChar(m_session, false);
-	if(m_plyr == NULL)
-		return false;
+	uint32 raceid = 0;
 
-	uint8 raceid = 0;
-	uint8 classid = 0;
+	Player *m_plyr = getSelectedChar(m_session, false);
 
-	if(strlen(args) > 0)
+	if (m_plyr && args && strlen(args) < 2)
+	{
+		raceid = m_plyr->getRace();
+		switch (raceid)
+		{
+		case 1:
+			race = "human";
+		break;
+		case 2:
+			race = "orc";
+		break;
+		case 3:
+			race = "dwarf";
+		break;
+		case 4:
+			race = "nightelf";
+		break;
+		case 5:
+			race = "undead";
+		break;
+		case 6:
+			race = "tauren";
+		break;
+		case 7:
+			race = "gnome";
+		break;
+		case 8:
+			race = "troll";
+		break;
+		case 9:
+			race = "goblin";
+		case 10:
+			race = "bloodelf";
+		break;
+		case 11:
+			race = "draenei";
+		case 22:
+			race = "worgen";
+		break;
+		default:
+			return false;
+		break;
+		}
+	}
+	else if (m_plyr && args && strlen(args) > 2)
 	{
 		race = args;
+		arcemu_TOLOWER(race);
+
+		// Teleport to specific race
 		if(race == "human")
 			raceid = 1;
 		else if(race == "orc")
 			raceid = 2;
 		else if(race == "dwarf")
 			raceid = 3;
-		else if(race == "nightelf" || race == "ne")
+		else if(race == "nightelf")
 			raceid = 4;
 		else if(race == "undead")
 			raceid = 5;
@@ -176,42 +219,43 @@ bool ChatHandler::HandleStartCommand(const char* args, WorldSession* m_session)
 			raceid = 7;
 		else if(race == "troll")
 			raceid = 8;
-		else if(race == "bloodelf" || race == "be")
+		else if(race == "goblin")
+			raceid = 9;
+		else if(race == "bloodelf")
 			raceid = 10;
 		else if(race == "draenei")
 			raceid = 11;
-		else if(race == "deathknight" || race == "dk")
-			classid = 6;
+		else if(race == "worgen")
+			raceid = 22;
 		else
 		{
-			RedSystemMessage(m_session, "Invalid start location! Valid locations are: human, dwarf, gnome, nightelf, draenei, orc, troll, tauren, undead, bloodelf, deathknight");
+			RedSystemMessage(m_session, "Invalid start location! Valid locations are: human, dwarf, gnome, nightelf, draenei, orc, troll, tauren, undead, bloodelf");
 			return true;
 		}
 	}
 	else
 	{
-		raceid = m_plyr->getRace();
-		classid = m_plyr->getClass();
-		race = "his";
-	}
-	// find the first matching one
-	PlayerCreateInfo* info = NULL;
-	for(uint8 i = 1; i <= 11; i++)
-	{
-		info = objmgr.GetPlayerCreateInfo((raceid ? raceid : i), (classid ? classid : i));
-		if(info != NULL)
-			break;
-	}
-
-	if(info == NULL)
-	{
-		RedSystemMessage(m_session, "Internal error: Could not find create info for race %u and class %u.", raceid, classid);
 		return false;
 	}
 
-	GreenSystemMessage(m_session, "Teleporting %s to %s starting location.", m_plyr->GetName(), race.c_str());
+	// Try to find a class that works
+	PlayerCreateInfo *info = NULL;
+	for(uint32 i=1;i<11;i++)
+	{
+		 info = objmgr.GetPlayerCreateInfo(raceid, i);
+		 if(info != NULL) break;
+	}
+	
+	if(info == NULL)
+	{
+		RedSystemMessage(m_session, "Error! Couldn't find [playercreateinfo]");
+		return false;
+	}
 
-	m_plyr->SafeTeleport(info->mapId, 0, LocationVector(info->positionX, info->positionY, info->positionZ));
+
+	GreenSystemMessage(m_session, "Transporting player [%s] to his [%s]'s starting location...", m_plyr->GetName(), race.c_str());
+
+	m_session->GetPlayer()->SafeTeleport(info->mapId, 0, LocationVector(info->positionX, info->positionY, info->positionZ));
 	return true;
 }
 
@@ -239,17 +283,14 @@ bool ChatHandler::HandleInfoCommand(const char* args, WorldSession* m_session)
 		}
 	}
 	objmgr._playerslock.ReleaseReadLock();
-	GreenSystemMessage(m_session, "Server Revision: |r%sArcEmu %s/%s-%s-%s %s(www.arcemu.org)", MSG_COLOR_WHITE,
-		BUILD_HASH_STR, CONFIG, PLATFORM_TEXT, ARCH, MSG_COLOR_LIGHTBLUE);
-	GreenSystemMessage(m_session, "Server Uptime: |r%s", sWorld.GetUptimeString().c_str());
-	GreenSystemMessage(m_session, "Current Players: |r%d (%d GMs) (%u Peak)", count, gm, sWorld.PeakSessionCount);
-	GreenSystemMessage(m_session, "Active Thread Count: |r%u", ThreadPool.GetActiveThreadCount());
-	GreenSystemMessage(m_session, "Free Thread Count: |r%u", ThreadPool.GetFreeThreadCount());
-	GreenSystemMessage(m_session, "Average Latency: |r%.3fms", ((float)avg / (float)count));
-	GreenSystemMessage(m_session, "CPU Usage: %3.2f %%", sWorld.GetCPUUsage());
-	GreenSystemMessage(m_session, "RAM Usage: %4.2f MB", sWorld.GetRAMUsage());
-	GreenSystemMessage(m_session, "SQL Query Cache Size (World): |r%u queries delayed", WorldDatabase.GetQueueSize());
-	GreenSystemMessage(m_session, "SQL Query Cache Size (Character): |r%u queries delayed", CharacterDatabase.GetQueueSize());
+	SystemMessage(m_session, "Latency |r%.3fms", ((float)avg / (float)count));
+	SystemMessage(m_session, "Developed by Zerocool, WoWD, Milamba and Hendrik");
+	SystemMessage(m_session, "Uptime |r%s", sWorld.GetUptimeString().c_str());
+	SystemMessage(m_session, "Players Online |r%dplayers %d Gamemasters", count, gm);
+	SystemMessage(m_session, "Active Thread Count |r%u", ThreadPool.GetActiveThreadCount());
+	SystemMessage(m_session, "CPU Usage %3.2f %%", sWorld.GetCPUUsage());
+	SystemMessage(m_session, "WOWD Query Cache Size |r%u queries delayed", WorldDatabase.GetQueueSize());
+	SystemMessage(m_session, "CHAR Query Cache Size: |r%u queries delayed", CharacterDatabase.GetQueueSize());
 
 	return true;
 }
